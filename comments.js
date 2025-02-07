00c1d6af89c6afc48d873abc86cb76053b623aec
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2025-02-07 03:39:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// create a new server
+// create a new server
+const express = require('express');
+const app = express();
+// import the comments data
+const comments = require('./data/comments');
+// import the posts data
+const posts = require('./data/posts');
